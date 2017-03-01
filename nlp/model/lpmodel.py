648e5226def39be84b5555b418627ac53728156c
Andrew Lambe <andrew.lambe@polymtl.ca>
2017-03-01 12:30:52 -0500

    Updated QPModel and LPModel classes to take in an NLPModel and a point and automatically construct a QP or LP approximation to the NLP.

--- nlp/model/lpmodel.py
+++ nlp/model/lpmodel.py
@@ -2,16 +2,10 @@
 """Base classes for linear optimization models."""
 
 import logging
-import os
-import sys
 import numpy as np
 from nlp.model.kkt import KKTresidual
-from nlp.tools.decorators import deprecated, counter
-from nlp.tools.utils import where
-from pykrylov.linop.linop import LinearOperator, DiagonalOperator, \
-    ReducedLinearOperator
-from pykrylov.linop.blkop import BlockLinearOperator
-
+from nlp.model.nlpmodel import NLPModel
+from pykrylov.linop.linop import LinearOperator
 
 class LPModel(QPModel):
     u"""Generic class to represent a linear programming (LP) problem.
@@ -21,23 +15,76 @@ class LPModel(QPModel):
                 l ≤ x ≤ u.
     """
 
-    def __init__(self, c, A=None, name='GenericLP', **kwargs):
-        """Initialize a LP with linear term `c` and Jacobian `A`.
+    def __init__(self, name='GenericLP', **kwargs):
+        """Initialize an LP.
 
-        :parameters:
-            :c:   Numpy array to represent the linear objective
-            :A:   linear operator to represent the constraint matrix.
-                  It must be possible to perform the operations `A*x`
-                  and `A.T*y` for Numpy arrays `x` and `y` of appropriate size.
+        The LP may be created in two ways: as a set of vector and matrix-like
+        objects or using a given NLPModel and a given point `x`. The latter
+        is useful in SLP or SLQP algorithms where an LP approximation is
+        constructed and solved to improve the candidate solution of the NLP.
+
+        :keywords:
+
+            :fromOps:   A group of operator objects in the form (`c`,`A`) where
+                        `c` is a numpy array and `A` is a matrix-like object
+                        (i.e., dense matrix, sparse matrix, or linear operator).
+
+            :fromProb:  A group consisting of an NLPModel (of any type) and a
+                        numpy array containing the point `x` at which the LP
+                        approximation is formed. If `x` is
+                        type `None`, a starting point of zero is used.
 
         See the documentation of `NLPModel` for futher information.
         """
-        n = c.shape[0]
+
+        fromOps = kwargs.get('fromOps',None)
+        fromProb = kwargs.get('fromProb',None)
+
+        if fromOps is not None:
+            c = fromOps[0]
+            A = fromOps[1]
+
+            # Basic data type checks.
+            n = c.shape[0]
+
+            if A is None:
+                raise ValueError('Cannot have an unconstrained LP')
+            else:
+                if A.shape[1] != n:
+                    raise ValueError('A has inconsistent shape')
+                m = A.shape[0]
+
+        elif fromProb is not None:
+            model = fromProb[0]
+            x = fromProb[1]
+
+            if x is None:
+                x = np.zeros(model.n, dtype=np.float)
+
+            # Evaluate model functions to construct the QP at x
+            c = model.grad(x)
+            A = model.jac(x)
+
+            n = c.shape[0]
+            m = A.shape[0]
+
+            # Make sure variable and constraint bounds are identified
+            kwargs['Lvar'] = model.Lvar
+            kwargs['Uvar'] = model.Uvar
+            kwargs['Lcon'] = model.Lcon
+            kwargs['Ucon'] = model.Ucon
+
+        else:
+            raise ValueError('LP could not be created.')
+
+        # Create a zero Hessian operator and use the QP model constructor
+        # to do the rest of the initialization work.
         H = LinearOperator(n, n,
                            lambda x: np.zeros(n),
                            symmetric=True,
                            dtype=np.float)
-        super(LPModel, self).__init__(c, H, A, name=name, **kwargs)
+        kwargs['fromOps'] = (c, H, A)
+        super(LPModel, self).__init__(name=name, **kwargs)
 
     def obj(self, x):
         """Evaluate the objective function at x."""
--- nlp/model/qpmodel.py
+++ nlp/model/qpmodel.py
@@ -2,15 +2,10 @@
 """Base classes for quadratic optimization models."""
 
 import logging
-import os
-import sys
 import numpy as np
 from nlp.model.kkt import KKTresidual
-from nlp.tools.decorators import deprecated, counter
-from nlp.tools.utils import where
-from pykrylov.linop.linop import LinearOperator, DiagonalOperator, \
-    ReducedLinearOperator
-from pykrylov.linop.blkop import BlockLinearOperator
+from nlp.model.nlpmodel import NLPModel
+from pykrylov.linop.linop import LinearOperator
 
 
 class QPModel(NLPModel):
@@ -21,40 +16,86 @@ class QPModel(NLPModel):
                 l ≤ x ≤ u.
     """
 
-    def __init__(self, c, H, A=None, name='GenericQP', **kwargs):
-        """Initialize a QP with linear term `c`, Hessian `H` and Jacobian `A`.
+    def __init__(self, name='GenericQP', **kwargs):
+        """Initialize a QP.
 
-        :parameters:
-            :c:   Numpy array to represent the linear objective
-            :A:   linear operator to represent the constraint matrix.
-                  It must be possible to perform the operations `A*x`
-                  and `A.T*y` for Numpy arrays `x` and `y` of appropriate size.
-                  If `A` is `None`, it will be replaced with an empty linear
-                  operator.
-            :H:   linear operator to represent the objective Hessian.
-                  It must be possible to perform the operation `H*x`
-                  for a Numpy array `x` of appropriate size. The operator `H`
-                  should be symmetric.
+        The QP may be created in two ways: as a set of vector and matrix-like
+        objects or using a given NLPModel and a given point `x`. The latter
+        is useful in SQP algorithms where a QP approximation is constructed and
+        solved to improve the candidate solution of the NLP.
+
+        :keywords:
+
+            :fromOps:   A group of operator objects in the form (`c`,`H`,`A`)
+                        where `c` is a numpy array, and `H` and `A` are
+                        dense matrices, sparse matrices, or linear operators.
+                        In addition, `A` may be type `None` to model
+                        unconstrained problems.
+
+                        Note: if `H` is a zero operator, the LPModel class
+                        should be used instead.
+
+            :fromProb:  A group consisting of an NLPModel (of any type) and a
+                        numpy array containing the point `x` at which the
+                        QP approximation to the NLPModel is formed. If `x` is
+                        type `None`, a starting point of zero is used.
 
         See the documentation of `NLPModel` for futher information.
         """
-        # Basic checks.
-        n = c.shape[0]
-        if A is None:
-            m = 0
-            self.A = LinearOperator(n, 0,
-                                    lambda x: np.empty((0, 1)),
-                                    matvec_transp=lambda y: np.empty((n, 0)),
-                                    dtype=np.float)
-        else:
-            if A.shape[1] != n or H.shape[0] != n or H.shape[1] != n:
-                raise ValueError('Shapes are inconsistent')
-            m = A.shape[0]
-            self.A = A
 
+        fromOps = kwargs.get('fromOps',None)
+        fromProb = kwargs.get('fromProb',None)
+
+        if fromOps is not None:
+            c = fromOps[0]
+            H = fromOps[1]
+            A = fromOps[2]
+
+            # Basic data type checks.
+            n = c.shape[0]
+            if H.shape[0] != n or H.shape[1] != n:
+                raise ValueError('H has inconsistent shape')
+
+            if A is None:
+                m = 0
+                A = LinearOperator(n, 0,
+                                   lambda x: np.empty((0, 1)),
+                                   matvec_transp=lambda y: np.empty((n, 0)),
+                                   dtype=np.float)
+            else:
+                if A.shape[1] != n:
+                    raise ValueError('A has inconsistent shape')
+                m = A.shape[0]
+
+        elif fromProb is not None:
+            model = fromProb[0]
+            x = fromProb[1]
+
+            if x is None:
+                x = np.zeros(model.n, dtype=np.float)
+
+            # Evaluate model functions to construct the QP at x
+            c = model.grad(x)
+            H = model.hess(x)
+            A = model.jac(x)
+
+            n = c.shape[0]
+            m = A.shape[0]
+
+            # Make sure variable and constraint bounds are identified
+            kwargs['Lvar'] = model.Lvar
+            kwargs['Uvar'] = model.Uvar
+            kwargs['Lcon'] = model.Lcon
+            kwargs['Ucon'] = model.Ucon
+
+        else:
+            raise ValueError('QP could not be created.')
+
+        # Initialize model and store key objects
         super(QPModel, self).__init__(n=n, m=m, name=name, **kwargs)
         self.c = c
         self.H = H
+        self.A = A
 
         # Default classification of constraints
         self._lin = range(self.m)             # Linear    constraints
@@ -82,10 +123,6 @@ class QPModel(NLPModel):
         if isinstance(self.A, np.ndarray):
             return np.dot(self.A, x)
         return self.A * x
-
-    def A(self, x):
-        """Evaluate the constraints Jacobian at x."""
-        return self.A
 
     def jac(self, x):
         """Evaluate the constraints Jacobian at x."""
